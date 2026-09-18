867bc04150d114efc6134cc2e7ec90939614d083
GHDB ASHEN <ghdbashen@gmail.com>
2026-09-18 17:35:45 +0530

    test 8

--- cloudpredict/setup.py
+++ cloudpredict/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='cloudpredict',
-    version='0.2.7',
+    version='0.2.8',
     description='Cross-platform serverless performance & cost predictor (AWS / Azure / GCP)',
     packages=find_packages(),
     include_package_data=True,
